9fbb8e4ccd6d2ce7ef362e7b70f62ad4963487ef
fawgv <fawgv@mail.ru>
2018-11-25 00:34:31 +0500

    После проведения задания

---
+++ cs/BowlingGame/Frame.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingGame
+{
+    public class Frame
+    {
+        public bool IsComplete { get; private set; }
+        public int Score { get; private set; }
+
+        private int rollCount;
+
+        public Frame()
+        {
+
+        }
+
+        public void AddRoll(int pins, bool endFrameGame = false)
+        {
+            Score += pins;
+            rollCount++;
+            if (!endFrameGame)
+            {
+                IsComplete = rollCount == 2 ? true : false;
+            }
+            else
+            {
+                IsComplete = rollCount == 3 ? true : false;
+            }
+        }
+    }
+}
--- cs/BowlingGame/Game.cs
+++ cs/BowlingGame/Game.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using BowlingGame.Infrastructure;
 using FluentAssertions;
 using NUnit.Framework;
@@ -7,25 +8,151 @@ namespace BowlingGame
 {
     public class Game
     {
+        private int currentFrameIndex;
+        private Frame[] Frames { get; set; }
+        private bool previousWasSpare = false;
+
+        public Game()
+        {
+            FramesInitialization();
+        }
+
+        private void FramesInitialization()
+        {
+            Frames = new Frame[10];
+            for (var i = 0; i < Frames.Length; i++)
+            {
+                Frames[i] = new Frame();
+            }
+        }
+
         public void Roll(int pins)
         {
+            if (pins<=10)
+            {
+                VerifyFrameIsFull();
+                Frames[currentFrameIndex].AddRoll(pins);
+            }
+            else
+            {
+                throw new ArgumentException("Can't roll more than 10");
+            }
+        }
+
+        private void VerifyFrameIsFull()
+        {
+            if (Frames[currentFrameIndex].IsComplete)
+            {
+                currentFrameIndex++;
+            }
         }
 
         public int GetScore()
         {
-            throw new NotImplementedException();
+            return Frames.Sum(frame => frame.Score);
         }
+
+        public int GetFrameScore(int indexFrame)
+        {
+            if (indexFrame >= Frames.Length)
+                throw new IndexOutOfRangeException("Can't have more than 10 frames");
+            return Frames[indexFrame].Score;
+        }
+
+        public int GetCurrentFrameIndex()
+        {
+            return currentFrameIndex;
+        }
+
+
+
     }
 
     [TestFixture]
     public class Game_should : ReportingTest<Game_should>
     {
+        private Game game;
+        private int score;
+
+        [SetUp]
+        public void SetUp()
+        {
+            game = new Game();
+        }
+
         [Test]
         public void HaveZeroScore_BeforeAnyRolls()
         {
-            new Game()
+            game
                 .GetScore()
                 .Should().Be(0);
         }
+
+        [Test]
+        public void HaveScore_AfterRoll()
+        {
+            game.Roll(6);
+            game
+                .GetScore()
+                .Should()
+                .Be(6);
+        }
+
+        [Test]
+        public void FrameHasZeroScore()
+        {
+ 
+            game
+                .GetFrameScore(0)
+                .Should().Be(0);
+        }
+
+        [Test]
+        public void CantHaveMoreThan10Frames()
+        {
+            var ex = Assert.Throws<IndexOutOfRangeException>(() => game.GetFrameScore(11));
+            Assert.AreEqual(ex.Message, "Can't have more than 10 frames");
+        }
+
+        [Test]
+        public void CantRollMoreThan10()
+        {
+            var ex = Assert.Throws<ArgumentException>(() => game.Roll(11));
+            Assert.AreEqual(ex.Message, "Can't roll more than 10");
+        }
+
+        [Test]
+        public void FrameScore10()
+        {
+            game.Roll(10);
+            game
+                .GetFrameScore(0)
+                .Should().Be(10);
+        }
+
+        [Test]
+        public void ChangesCurrentFrameIndex()
+        {
+            game.Roll(3);
+            game.Roll(5);
+
+            game.Roll(4);
+
+            game.GetCurrentFrameIndex().Should().Be(1);
+        }
+
+        [Test]
+        public void SpareBonus()
+        {
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(3);
+
+            game.GetFrameScore(0)
+                .Should()
+                .Be(13);
+        }
+
+        
     }
 }
